87c6e376cd8b7767d10e35845d21f7b5b3ce6582
arjunsaseendran91 <arjunsaseendran@icloud.com>
2024-06-13 13:41:54 +0530

    project created with vite

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <form action="">
+      <h1>fill in the otp</h1>
+      <div>
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+      </div>
+      <button>Submit</button>
+    </form>
+  );
+}
+
+export default App;
